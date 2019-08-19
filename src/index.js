108e3e40a1c22b90e9f909c88db6b779b5137b39
Kyle Clabough <kaclabough@gmail.com>
2019-08-19 15:13:28 -0400

    src/index.js routes to src/route/index.js

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ const cors = require("cors");
 app.use(parser.json());
 app.use(cors());
 
-app.use("/", require("./route/app"));
-app.use("/api/anime", require("./route/anime"));
+app.use("/", require("./route/index"));
 
 app.set("port", process.env.PORT || 3001);
 
