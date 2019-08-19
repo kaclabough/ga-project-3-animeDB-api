5d236cb66a180b2884f27b50db4f99778430ab46
Kyle Clabough <kaclabough@gmail.com>
2019-08-19 17:14:21 -0400

    anime routers completed

--- src/controllers/anime.js
+++ src/controllers/anime.js
@@ -5,5 +5,22 @@ module.exports = {
     Anime.find({}).then(anime => {
       res.json(anime);
     });
+  },
+  findId: (req, res) => {
+    Anime.find({ _id: req.params.id }).then(anime => {
+      res.json(anime);
+    });
+  },
+  update: (req, res) => {
+    Anime.findAndUpdate({ title: { en: req.params.title } }, req.body).then(
+      anime => {
+        res.json(anime);
+      }
+    );
+  },
+  create: (req, res) => {
+    Anime.create(req.body).then(anime => {
+      res.json(anime);
+    });
   }
 };
--- src/route/anime.js
+++ src/route/anime.js
@@ -3,5 +3,7 @@ const router = express.Router();
 const animeController = require("../controllers/anime");
 
 router.get("/", animeController.index);
-
+router.get("/id/:id", animeController.findId);
+router.post("/new", animeController.create);
+router.put("/title/:title", animeController.update);
 module.exports = router;
