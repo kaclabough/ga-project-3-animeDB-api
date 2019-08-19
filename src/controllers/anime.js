6db27de25e6e348b98fcd4b558eee1debec9f325
Kyle Clabough <kaclabough@gmail.com>
2019-08-19 15:24:54 -0400

    cleaned up code

--- src/controllers/anime.js
+++ src/controllers/anime.js
@@ -2,7 +2,6 @@ const Anime = require("../db/models/Anime.js");
 
 module.exports = {
   index: (req, res) => {
-    console.log(res);
     Anime.find({}).then(anime => {
       res.json(anime);
     });
