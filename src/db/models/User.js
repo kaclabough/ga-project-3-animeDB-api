de72d29fa6091b419d33fdab403eaf6f55034173
Kyle Clabough <kaclabough@gmail.com>
2019-08-19 16:23:41 -0400

    add currentlyWatching property to User model

--- src/db/models/User.js
+++ src/db/models/User.js
@@ -4,7 +4,8 @@ const Schema = mongoose.Schema;
 const User = new Schema({
   userName: String,
   email: String,
-  favorites: [{ type: mongoose.Schema.Types.ObjectID, ref: "Anime" }]
+  favorites: [{ type: mongoose.Schema.Types.ObjectId, ref: "Anime" }],
+  currentlyWatching: [{ type: mongoose.Schema.Types.ObjectId, ref: "Anime" }]
 });
 
 module.exports = mongoose.model("User", User);
