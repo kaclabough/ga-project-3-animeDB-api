ea095262a81cf592a6cccf92f370ba14e4158326
Kyle Clabough <kaclabough@gmail.com>
2019-08-20 10:08:38 -0400

    added update by id router

--- src/route/anime.js
+++ src/route/anime.js
@@ -5,5 +5,7 @@ const animeController = require("../controllers/anime");
 router.get("/", animeController.index);
 router.get("/id/:id", animeController.findId);
 router.post("/new", animeController.create);
-router.put("/title/:title", animeController.update);
+router.put("/title/:title", animeController.updateTitle);
+router.put("/id/:id", animeController.updateId);
+
 module.exports = router;
