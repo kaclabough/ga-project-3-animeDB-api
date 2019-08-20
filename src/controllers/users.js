3d419b2d76529746c00cfa7424182203b278c291
KeeneWong <keenewonghofung@gmail.com>
2019-08-20 00:09:23 -0400

    user signup

---
+++ src/controllers/users.js
@@ -0,0 +1,44 @@
+const express = require("express");
+const jwt = require("jwt-simple");
+const passport = require("../../config/passport");
+const config = require("../../config/config");
+const mongoose = require("../db/models/User");
+const User = mongoose.model("User");
+const router = express.Router();
+
+router.delete("/:id", (req, res) => {
+  User.findByIdAndDelete(req.params.id).then(deleted => res.send(deleted));
+});
+
+router.post("/signup", (req, res) => {
+  if (req.body.email && req.body.password) {
+    let newUser = {
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password
+    };
+    User.findOne({ email: req.body.email }).then(user => {
+      if (!user) {
+        User.create(newUser).then(user => {
+          if (user) {
+            var payload = {
+              id: newUser.id
+            };
+            var token = jwt.encode(payload, config.jwtSecret);
+            res.json({
+              token: token
+            });
+          } else {
+            res.sendStatus(401);
+          }
+        });
+      } else {
+        res.sendStatus(401);
+      }
+    });
+  } else {
+    res.sendStatus(401);
+  }
+});
+
+module.exports = router;
